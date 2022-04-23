cd7a9962fc8d64944540737ca39a9f5debabf3cd
Daison Carino <daison12006013@gmail.com>
2022-04-24 00:41:18 +0800

    Move sveltekit-starter as views/svelte

--- cmd/build-svelte.sh
+++ cmd/build-svelte.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 (
-    cd resources/sveltekit-starter/ \
+    cd resources/views/svelte/ \
     && rm -rf .svelte-kit/ \
     && npm install \
     && npm audit fix \
     && npm run build \
 ) && \
-cp -Rf resources/sveltekit-starter/node_modules .build/sveltekit/
-cp -Rf resources/sveltekit-starter/package.json .build/sveltekit/
+cp -Rf resources/views/svelte/node_modules .build/sveltekit/
+cp -Rf resources/views/svelte/package.json .build/sveltekit/
